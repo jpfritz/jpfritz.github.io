16dce348f08d572a89da1d8e8c0cd308a25eac83
jpfritz <jneukamm@gmx.net>
2014-07-29 14:41:24 +0200

    new

--- angularTut/07-modules/app.js
+++ angularTut/07-modules/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
-angular.module('tutorialApp', []);
+angular.module('tutorialApp', [])
+  .controller('ArticlesCtrl', function($scope, $http){
+    $http.get('articles.json').then(function(articlesResponse) {
+	  $scope.articles = articlesResponse.data;
+	});
+  });
